32b66fdcea2383919f039d1ed9a52dd558f8bc31
lbodor <lazar.bodor@ga.gov.au>
2017-10-25 09:33:18 +1100

    Minor refactor and tidy up

--- src/test/java/au/gov/ga/geodesy/support/marshalling/moxy/GeodesyMLMoxyTest.java
+++ src/test/java/au/gov/ga/geodesy/support/marshalling/moxy/GeodesyMLMoxyTest.java
@@ -27,33 +27,24 @@ public class GeodesyMLMoxyTest {
         .add("geo", "urn:xml-gov-au:icsm:egeodesy:0.4")
         .add("gml", "http://www.opengis.net/gml/3.2");
     
-    private GeodesyMLMoxy marshaller; public GeodesyMLMoxyTest() throws MarshallingException {
+    private GeodesyMLMoxy marshaller;
+
+    public GeodesyMLMoxyTest() throws MarshallingException {
         marshaller = new GeodesyMLMoxy();
     }
 
     @Test
     public void unmarshal() throws Exception {
-        Reader input = new InputStreamReader(Thread.currentThread()
-            .getContextClassLoader()
-            .getResourceAsStream("MOBS.xml"));
-
+        Reader input = getSiteLogFile("MOBS.xml");
         GeodesyMLType geodesyML = marshaller.unmarshal(input, GeodesyMLType.class).getValue();
         List<JAXBElement<?>> els = geodesyML.getElements();
         Assert.assertNotNull(els);
         Assert.assertNotEquals(0, els.size());
-
-        System.out.println("geodesyML elements:");
-        geodesyML.getElements().forEach(x -> {
-            System.out.println("  "+x.getName());
-        });
     }
 
     @Test
     public void generateGmlId() throws Exception {
-        Reader input = new InputStreamReader(Thread.currentThread()
-            .getContextClassLoader()
-            .getResourceAsStream("MOBS.xml"));
-
+        Reader input = getSiteLogFile("MOBS.xml");
         GeodesyMLType geodesyML = marshaller.unmarshal(input, GeodesyMLType.class).getValue();
         SiteType siteLogType = (SiteType) geodesyML.getElements().get(0).getValue();
         siteLogType.setId(null);
@@ -65,10 +56,7 @@ public class GeodesyMLMoxyTest {
 
     @Test
     public void unmarshalWithNullNumericFields() throws Exception {
-        Reader input = new InputStreamReader(Thread.currentThread()
-                .getContextClassLoader()
-                .getResourceAsStream("MOBS-null-numerics.xml"));
-
+        Reader input = getSiteLogFile("MOBS-null-numerics.xml");
         GeodesyMLType geodesyML = marshaller.unmarshal(input, GeodesyMLType.class).getValue();
         List<JAXBElement<?>> els = geodesyML.getElements();
         Assert.assertNotNull(els);
@@ -76,11 +64,11 @@ public class GeodesyMLMoxyTest {
 
         geodesyML.getElements().forEach(x -> {
             if (x.getName().toString().indexOf("humiditySensor") != -1) {
-                HumiditySensorType humiditySensorType = (HumiditySensorType)x.getValue();
+                HumiditySensorType humiditySensorType = (HumiditySensorType) x.getValue();
 
                 // verify that a non-null Double field has the correct value
                 Double heightDiffToAntenna = humiditySensorType.getHeightDiffToAntenna();
-                Assert.assertEquals((Double)2.5, (Double)heightDiffToAntenna);
+                Assert.assertEquals((Double) 2.5, (Double) heightDiffToAntenna);
 
                 // verify that a null Double field is null in the type (not zero)
                 Double accuracyPercentRelativeHumidity = humiditySensorType.getAccuracyPercentRelativeHumidity();
@@ -91,20 +79,21 @@ public class GeodesyMLMoxyTest {
 
     @Test
     public void marshalJAXBElement() throws Exception {
-        Reader input = new InputStreamReader(Thread.currentThread()
-            .getContextClassLoader()
-            .getResourceAsStream("MOBS.xml"));
-
-        marshaller.marshal(marshaller.unmarshal(input, GeodesyMLType.class), new PrintWriter(System.out));
+        Reader input = getSiteLogFile("MOBS.xml");
+        JAXBElement<GeodesyMLType> ml = marshaller.unmarshal(input, GeodesyMLType.class);
+        marshaller.marshal(ml, new PrintWriter(System.out));
     }
 
     @Test
     public void marshal() throws Exception {
-        Reader input = new InputStreamReader(Thread.currentThread()
-            .getContextClassLoader()
-            .getResourceAsStream("MOBS.xml"));
-
+        Reader input = getSiteLogFile("MOBS.xml");
         GeodesyMLType ml = marshaller.unmarshal(input, GeodesyMLType.class).getValue();
         marshaller.marshal(ml, new PrintWriter(System.out));
+    }
+
+    private Reader getSiteLogFile(String fileName) {
+        return new InputStreamReader(Thread.currentThread()
+            .getContextClassLoader()
+            .getResourceAsStream(fileName));
     }
 }
