8773f698642314f050915e4177ccb690d30408d3
lbodor <lazar.bodor@ga.gov.au>
2019-04-18 14:31:48 +1000

    Add @XmlSchema to OGC jaxb packages

--- src/main/java/net/opengis/gml/v_3_2_1/package-info.java
+++ src/main/java/net/opengis/gml/v_3_2_1/package-info.java
@@ -1,8 +1,15 @@
+@XmlSchema(
+  namespace = "http://www.opengis.net/gml/3.2",
+  elementFormDefault = XmlNsForm.QUALIFIED
+)
+
 @XmlJavaTypeAdapters({
-    @XmlJavaTypeAdapter(value=StringAdapter.class, type=String.class)
+    @XmlJavaTypeAdapter(value = StringAdapter.class, type = String.class)
 })
 package net.opengis.gml.v_3_2_1;
 
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
 
