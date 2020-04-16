e13bec4b571b18c4a33e70e6b32db7d2c4ecb239
Frank Fu <frank.fu@ga.gov.au>
2020-04-16 11:39:02 +1000

    Modify travis/build.sh to deploy geodesyml v0.4 into artifactory and AWS without merging

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+#if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     nix-shell --command "mvn --update-snapshots deploy"
-else
-    nix-shell --command "mvn --update-snapshots test"
-fi
+#else
+#    nix-shell --command "mvn --update-snapshots test"
+#fi
